b21b1dd1a878914644fd79b2a12344472469ebce
heefan <heefan@gmail.com>
2016-06-15 00:15:57 +0800

    remove output

--- heefan/android/GoogleTest/obj/local/arm64-v8a/objs/foo/foo.o.d
+++
@@ -1,2 +0,0 @@
-/Users/litian/heefan/github/googletest/heefan/android/GoogleTest/obj/local/arm64-v8a/objs/foo/foo.o: \
- /Users/litian/heefan/github/googletest/heefan/android/GoogleTest/jni/foo.cpp
--- heefan/android/GoogleTest/obj/local/mips/objs/foo/foo.o.d
+++
@@ -1,2 +0,0 @@
-/Users/litian/heefan/github/googletest/heefan/android/GoogleTest/obj/local/mips/objs/foo/foo.o: \
- /Users/litian/heefan/github/googletest/heefan/android/GoogleTest/jni/foo.cpp
