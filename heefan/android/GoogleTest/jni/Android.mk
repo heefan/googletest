0b013dfa71cd9c123b49fb96d3806d09d198419b
heefan <heefan@gmail.com>
2016-06-15 18:01:49 +0800

    test gmock

--- heefan/android/GoogleTest/jni/Android.mk
+++ heefan/android/GoogleTest/jni/Android.mk
@@ -1,15 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := foo
-LOCAL_SRC_FILES := foo.cpp
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_MODULE := foo      # library name
+LOCAL_SRC_FILES := foo.cpp    # source code
+include $(BUILD_SHARED_LIBRARY) # build as so file, libfoo.so
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := foo_unittest
-LOCAL_SRC_FILES := foo_unittest.cpp
-LOCAL_SHARED_LIBRARIES := foo
-LOCAL_STATIC_LIBRARIES := googletest_main
-include $(BUILD_EXECUTABLE)
+LOCAL_MODULE := foo_unittest   # exe name
+LOCAL_SRC_FILES := foo_unittest.cpp # exe source code
+LOCAL_SHARED_LIBRARIES := foo  # link library libfoo.so
+LOCAL_STATIC_LIBRARIES := googletest_main # link library  libgoogletest_main.a
+LOCAL_STATIC_LIBRARIES += gmock_main # link library  libgoogletest_main.a
 
-$(call import-module,third_party/googletest)
+include $(BUILD_EXECUTABLE)    
+
+$(call import-module,third_party/googletest)   
+$(call import-module,third_party/gmock)
+
